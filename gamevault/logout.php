2a766144d7b9cb0019c0e54926bcb0223f694b12
nessim <nessim.ouiazem@ynov.com>
2026-03-16 09:05:49 +0100

    feat: first commit

--- gamevault/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/config.php';
-session_destroy();
-setcookie(session_name(), '', time() - 3600, '/');
-session_start();
-setFlash('success', 'Vous avez été déconnecté. À bientôt !');
-redirect(BASE_URL . '/login.php');
